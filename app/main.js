cf2f6c8e659314574ceccc49444c26390bd1495a
Karthikeyan k <pgmrkarthik@gmail.com>
2020-02-26 14:56:29 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,33 +1,34 @@
 
-window.onload = function() {
-  var onload = true;
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]){
-          if(onload){
-            var Model3D = document.getElementById("model-viewer");
-            console.log(Model3D)
-            model_src = Model3D.src;
-            Model3D.remove(); 
-            onload=false;
-          }
-            var Model2 = document.getElementById("dmodel-viewer")
-            var model= URL.createObjectURL(this.files[0]);
-            var model_viewertag='<model-viewer ar src="'+model+'" shadow-intensity="1" auto-rotate camera-controls alt=""></model-viewer>'
-            Model2.innerHTML = model_viewertag;
-        }
-    })
-    var dropbox=document.getElementById("dropBox")
-    dropbox.addEventListener('ondrop',function(){
-      e.stopPropagation();
-        e.preventDefault();
-    
-        // Retrieve the dragged data by type
-        var data = e.dataTransfer.getData("Text");
-    
-        // Append image to the drop box
-        e.target.appendChild(document.getElementById(data));
-      console.log("Hello")
-    })
-};
-
-
+window.onload = function(){
+  document.getElementById('fileInput').addEventListener('change', function () {
+    if (this.files && this.files[0]) {
+      if ( document.getElementById("model-viewer")) {
+        document.getElementById("model-viewer").remove();
+      }
+      var modelNameTag =document.getElementById("model-name");
+      var modelName = this.files[0].name;
+      modelName = modelName.substring(0,modelName.indexOf('.'))
+      modelNameTag.innerText = modelName
+      var Model2 = document.getElementById("dmodel-viewer")
+      var model = URL.createObjectURL(this.files[0]);
+      var model_viewertag = '<model-viewer ar src="' + model + '" shadow-intensity="1" auto-rotate camera-controls alt=""></model-viewer>'
+      Model2.innerHTML = model_viewertag;
+    }
+  })
+  document.addEventListener('ondrop', function (event) {
+   var droppedFiles = event.target.files || event.dataTransfer.files;
+    if (droppedFiles && droppedFiles[0]) {
+      if ( document.getElementById("model-viewer")) {
+        document.getElementById("model-viewer").remove();
+      }
+      var modelNameTag =document.getElementById("model-name");
+      var modelName = files[0].name;
+      modelName = modelName.substring(0,modelName.indexOf('.'))
+      modelNameTag.innerText = modelName
+      var Model2 = document.getElementById("dmodel-viewer")
+      var model = URL.createObjectURL(droppedFiles[0]);
+      var model_viewertag = '<model-viewer ar src="' + model + '" shadow-intensity="1" auto-rotate camera-controls alt=""></model-viewer>'
+      Model2.innerHTML = model_viewertag;
+    }
+  })
+}
